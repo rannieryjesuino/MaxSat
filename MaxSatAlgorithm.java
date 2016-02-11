2a34dc462f72c605a0ea6906d7f4d33f60caaf05
Ranniery Jesuino de Oliveira <ranniery.jesuino@gmail.com>
2016-02-11 10:04:10 -0300

    Primeiro commit! o/

---
+++ MaxSatAlgorithm.java
@@ -0,0 +1,22 @@
+package br.uece.lotus.tools.MaxSat;
+
+import br.uece.lotus.Component;
+import br.uece.lotus.State;
+import br.uece.lotus.Transition;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Ranniery on 11/02/2016.
+ */
+
+public class MaxSatAlgorithm {
+
+    public static void Magic {
+        JOptionPane.showMessageDialog(null, "Is magic!",
+                "Friendship", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
---
+++ MaxSatPlugin.java
@@ -0,0 +1,33 @@
+package br.uece.lotus.tools.MaxSat;
+
+import br.uece.lotus.Component;
+import br.uece.lotus.State;
+import br.uece.lotus.Transition;
+import br.uece.lotus.project.ProjectExplorer;
+import br.uece.seed.app.UserInterface;
+import br.uece.seed.ext.ExtensionManager;
+import br.uece.seed.ext.Plugin;
+import br.uece.lotus.tools.MaxSat.MaxSatAlgorithm;
+
+/**
+ * Created by Ranniery on 11/02/2016.
+ */
+
+public class MaxSatPlugin extends Plugin{
+
+    private UserInterface mUserInterface;
+    private ProjectExplorer mProjectExplorer;
+
+    @Override
+    public void onStart(ExtensionManager extensionManager) throws Exception {
+        mUserInterface = (UserInterface) extensionManager.get(UserInterface.class);
+        mProjectExplorer = (ProjectExplorer) extensionManager.get(ProjectExplorer.class);
+
+        mUserInterface.getMainMenu().newItem("MaxSat")
+                .setWeight(Integer.MAX_VALUE)
+                .setAction(() -> {
+                    MaxSatAlgorithm.Magic();
+                })
+                .create();
+
+}
